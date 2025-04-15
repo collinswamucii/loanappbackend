7b6251c327b2970a3dcf9d00631877085aaced59
collinswamucii <collinswamucii@gmail.com>
2025-04-15 15:03:19 +0300

    Initiated variables to the Repo files.

--- src/main/java/com/app/loanapp/repository/LoanRepository.java
+++ src/main/java/com/app/loanapp/repository/LoanRepository.java
@@ -1,11 +1,9 @@
 package com.app.loanapp.repository;
 
 import com.app.loanapp.entity.Loan;
-import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-@Id
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
     List<Loan> findByCustomerId(Long customerId);
